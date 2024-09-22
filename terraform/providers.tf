b331448494d5e40084c70482fd01a3124fe4c6ef
Olohungbebe kehinde Sa'adudeen <kehindesaad@gmail.com>
2024-09-22 20:51:20 +0100

    modify

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -1,7 +1,6 @@
 # providers.tf
 provider "azurerm" {
   features {}
-  subscription_id = "ba85713b-a9b8-4e7c-a373-da0bf7a68327"
 }
 
 provider "helm" {
@@ -12,3 +11,4 @@ provider "helm" {
     cluster_ca_certificate = base64decode(azurerm_kubernetes_cluster.aks-istio-cluster.kube_config[0].cluster_ca_certificate)
   }
 }
+
