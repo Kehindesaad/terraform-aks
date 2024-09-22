51a9c7d6d5ccbac80ee60f07e6aa294a7919ee66
Olohungbebe kehinde Sa'adudeen <kehindesaad@gmail.com>
2024-09-22 20:27:33 +0100

    modify

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,13 +1,13 @@
 # main.tf
 module "networking" {
-  source = "terraform/network"
+  source = "./terraform/network"
   resource_group_name = var.resource_group_name
   location = var.location
   aks_subnet_id = module.networking.aks_subnet_id
 }
 
 module "aks" {
-  source = "terraform/aks"
+  source = "./terraform/aks"
   aks_subnet_id = module.networking.aks_subnet_id
   resource_group_name = var.resource_group_name
   location = var.location
@@ -15,7 +15,7 @@ module "aks" {
 
 
 module "storage" {
-  source = "terraform/storage"
+  source = "./terraform/storage"
   resource_group_name = var.resource_group_name
   location = var.location
 }
