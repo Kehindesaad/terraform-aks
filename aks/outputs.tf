f7d1e5abc97f4f650f61d9b629138895821da680
Olohungbebe kehinde Sa'adudeen <kehindesaad@gmail.com>
2024-09-23 11:22:32 +0100

    modify

--- aks/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "cluster_name" {
-  value = azurerm_kubernetes_cluster.aks-istio-cluster.name
-}
-
-output "acr_login_server" {
-  value = azurerm_container_registry.saadudeenregistry247.login_server
-}
